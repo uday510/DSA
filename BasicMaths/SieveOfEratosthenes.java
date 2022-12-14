cc3f9a5c7bc39855d779132010fcdc725722a0fc
Uday Teja <uday@Udays-MacBook.local>
2022-12-14 22:17:49 +0530

    Added codes:

--- BasicMaths/SieveOfEratosthenes.java
+++ BasicMaths/SieveOfEratosthenes.java
@@ -28,4 +28,20 @@ public class SieveOfEratosthenes {
         }
         return result;
     }
+    
+    /**
+     *  boolean[] notPrime = new boolean[n];
+     *         int count = 0;
+     *         for (int i = 2; i < n; i++) {
+     *             if (notPrime[i] == false) {
+     *                 count++;
+     *                 for (int j = 2; i*j < n; j++) {
+     *                     notPrime[i*j] = true;
+     *                 }
+     *             }
+     *         }
+     *
+     *         return count;
+     *     }
+     */
 }
