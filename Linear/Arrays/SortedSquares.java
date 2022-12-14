2e9ad3f1286513b418e51230261dfe872688e567
uday510 <admin@udayteja.com>
2022-12-15 00:49:30 +0530

    Added codes for Array

---
+++ Linear/Arrays/SortedSquares.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class SortedSquares {
+ public static void main(String[] args) {
+     int[] array = {1, 2, 3, 5, 6, 8, 9};
+     System.out.println(Arrays.toString(solve(array)));
+ }
+
+    public static int[] solve(int[] array) {
+     int smallerValueIdx = 0;
+     int largerValueIdx = array.length - 1;
+     int[] sortedSquares = new int[array.length];
+
+     for(int idx = array.length - 1; idx > -1;) {
+         int smallerValue = array[smallerValueIdx];
+         int largerValue = array[largerValueIdx];
+         if(Math.abs(smallerValue) > Math.abs(largerValue)) {
+             sortedSquares[idx] = smallerValue * smallerValue;
+             smallerValueIdx++;
+         } else {
+             sortedSquares[idx] = largerValue * largerValue;
+             largerValueIdx--;
+         }
+         idx--;
+     }
+        return sortedSquares;
+ }
+}
