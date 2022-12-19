c929afc7749facf1dd15fe33e59f55906deefb10
uday510 <admin@udayteja.com>
2022-12-19 23:06:52 +0530

    Added ThreeNumber Sum

--- Linear/Arrays/NonConstructibleChange.java
+++ Linear/Arrays/NonConstructibleChange.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class NonConstructibleChange {
+import java.util.Arrays;
+
+public class NonConstructibleChange {
+    public static void main(String[] args) {
+        int[] coins = {5, 7, 1, 1, 2, 3, 22};
+        System.out.println(solve(coins));
+    }
+    public static int solve(int[] coins) {
+
+        Arrays.sort(coins);
+        int currentChangeCreated = 0;
+
+        for(int coin: coins) {
+            if(coin > currentChangeCreated + 1) return currentChangeCreated + 1;
+            currentChangeCreated += coin;
+        }
+        return currentChangeCreated + 1;
+    }
+
 }
