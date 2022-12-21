d8a973871c63283d40feeae7623f1a489fd31848
uday510 <admin@udayteja.com>
2022-12-22 01:16:37 +0530

    Added Patterns

---
+++ Patterns/Pattern9.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n = 5;
+        solve(n);
+    }
+    public static void solve(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int l = i; l < n; l++)
+                System.out.print(" ");
+            for(int j = 1; j <= (2 * i) - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+        for(int i = n; i >= 1; i--) {
+            for(int k = i; k < n; k++) System.out.print(" ");
+            for(int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
