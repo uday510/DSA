76b88ee46b2fe82e45f8dcb39e28822b647bae8f
uday510 <admin@udayteja.com>
2022-12-24 15:13:58 +0530

    Added Patterns

--- Patterns/Pattern12.java
+++ Patterns/Pattern12.java
@@ -2,15 +2,15 @@ package Patterns;
 
 public class Pattern12 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         solve(n);
     }
     public static void solve(int n) {
 
        for(int i = 1; i <= n; i++) {
-           for(int j = 1; j <= i; j++) System.out.print(j+" ");
+           for(int j = 1; j <= i; j++) System.out.print(j);
            for(int k = 2 * (n - i); k > 0; k--) System.out.print(" ");
-           for(int l = i; l >= 1; l--) System.out.print(l+ " ");
+           for(int l = i; l >= 1; l--) System.out.print(l);
            System.out.println();
        }
     }
--- Patterns/Pattern18.java
+++ Patterns/Pattern18.java
@@ -2,7 +2,7 @@ package Patterns;
 
 public class Pattern18 {
     public static void main(String[] args) {
-        int i = 5;
+        int i = 4;
         solve(i);
     }
 
