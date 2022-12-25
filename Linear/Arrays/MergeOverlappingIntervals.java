3d4c479c069effa758e4cb1ef49e685503232740
uday510 <admin@udayteja.com>
2022-12-26 01:32:51 +0530

    Added Merged interval Problem

---
+++ Linear/Arrays/MergeOverlappingIntervals.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeOverlappingIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = { {1, 2},
+                              {3, 7},
+                              {4, 7},
+                              {6, 8},
+                              {9, 10} };
+        int[][] result = solve(intervals);
+        System.out.println(Arrays.deepToString(result));
+    }
+    public static int[][] solve(int[][] intervals) {
+        // O(nlog(n)) time | O(n) space
+        //Sort the intervals by starting value.
+        int[][] sortedIntervals = intervals.clone();
+        Arrays.sort(sortedIntervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int[] currentInterval = sortedIntervals[0];
+        mergedIntervals.add(currentInterval);
+
+        for(int[] nextInterval: sortedIntervals) {
+            int currentIntervalEnd = currentInterval[0];
+            int nextIntervalStart = nextInterval[0];
+            int nextIntervalEnd = nextInterval[1];
+
+            if(currentIntervalEnd >= nextIntervalStart) {
+                currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+            } else {
+                currentInterval = nextInterval;
+                mergedIntervals.add(currentInterval);
+            }
+        }
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
+    }
+}
