b7a40f709fbe6cdbdac94f59b329f4ff9ad969b3
uday510 <admin@udayteja.com>
2023-01-08 16:10:32 +0530

    Added Array of Products

--- Linear/Arrays/MoveElementToEnd.java
+++ Linear/Arrays/MoveElementToEnd.java
@@ -1,5 +1,22 @@
 import java.util.Arrays;
 
+/**
+ *
+
+ You're given an array of integers and an integer. Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
+
+ The function should perform this in place (i.e., it should mutate the input array) and doesn't need to maintain the order of the other integers.
+ Sample Input
+
+ array = [2, 1, 2, 2, 2, 3, 4, 2]
+ toMove = 2
+
+ Sample Output
+
+ [1, 3, 4, 2, 2, 2, 2, 2] // the numbers 1, 3, and 4 could be ordered differently
+
+
+ */
 public class MoveElementToEnd {
     public static void main(String[] args) {
         int[] array = {2, 1, 2, 2, 2, 3, 4, 2};
---
+++ production/DSA/production/DSA/Sorting/heapSort/minHeapConstruction.js
@@ -0,0 +1,48 @@
+class MinHeap {
+    constructor(array) {
+        this.heap = this.buildHeap(array);
+    }
+    //! O(n) time | O(1) space
+    buildHeap(array) {
+        const firstParentIdx = Math.floor( (array.length - 2) / 2);
+        for(let currentIdx = firstParentIdx; currentIdx>= 0; currentIdx--) {
+            this.siftDown(currentIdx, array.length - 1, array);
+        }
+        return array;
+    }
+    //! O(logn) time | O(1) space
+    siftDown(currentIdx, endIdx, heap) {
+        let childOneIdx = currentIdx * 2 + 1;
+        while(childOneIdx <= endIdx) {
+            const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+            let idxToSwap;
+            if(childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+                idxToSwap = childTwoIdx;
+            }
+			else {
+                idxToSwap = childOneIdx;
+            }
+            if(heap[idxToSwap] < heap[currentIdx]) {
+                this.swap(currentIdx, idxToSwap, heap);
+                currentIdx = idxToSwap;
+                childOneIdx = currentIdx * 2 + 1;
+            } else {
+                return;
+            }
+        }
+    }
+    // O(log(n)) time | O(1) space
+    siftUp(currentIdx, heap) {
+        let parentIdx = Math.floor( (currentIdx - 1) / 2);
+        while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+            this.swap(currentIdx, parentIdx, heap);
+            currentIdx = parentIdx;
+            parentIdx = Math.floor( (currentIdx - 1) / 2);
+        }
+    }
+
+    //! O(1) space
+    peek() {
+        return this.heap[0];
+    }
+
