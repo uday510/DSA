0f630a6e81c129ff06d1d920bd83605cbd987c75
uday510 <admin@udayteja.com>
2023-02-05 14:34:13 +0530

    Add Bitwise problems solution

---
+++ BitManipulation/XorOperationInArray.java
@@ -0,0 +1,60 @@
+/**
+ * You are given an integer n and an integer start.
+ *
+ * Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+ *
+ * Return the bitwise XOR of all elements of nums.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 5, start = 0
+ * Output: 8
+ * Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+ * Where "^" corresponds to bitwise XOR operator.
+ * Example 2:
+ *
+ * Input: n = 4, start = 3
+ * Output: 8
+ * Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 1000
+ * 0 <= start <= 1000
+ * n == nums.length
+ *
+ * Apple
+ * |
+ * 2
+ *
+ * Bloomberg
+ * |
+ * 2
+ *
+ * Walmart Global Tech
+ * |
+ * 1
+ */
+package BitManipulation;
+
+public class XorOperationInArray {
+
+    public static void main(String[] args) {
+        int start = 3, n = 4;
+        int res = solve(n, start);
+        System.out.println(res);
+    }
+    public static int solve(int n, int start) {
+        int res = 0, i = 0;
+
+        while (i < n) {
+            int currentNum = start + 2 * i;
+            res = res ^ currentNum;
+            i++;
+        }
+        return res;
+    }
+}
