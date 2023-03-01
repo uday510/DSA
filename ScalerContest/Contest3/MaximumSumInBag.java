92f6c8b30124c700fda8d45425d1099827771d74
uday510 <admin@udayteja.com>
2023-03-02 00:26:10 +0530

    Add Kadane's Algorithm

--- ScalerContest/Contest3/MaximumSumInBag.java
+++ ScalerContest/Contest3/MaximumSumInBag.java
@@ -4,37 +4,56 @@ import java.util.Arrays;
 
 public class MaximumSumInBag {
     public static void main(String[] args) {
-        int[] a = {16, 3, 3, 6, 7, 8, 17, 13, 7};
-        int [] b = {0, 1, 0, 1, 0, 0, 1, 1, 0};
+//        int[] a = {16, 3, 3, 6, 7, 8, 17, 13, 7};
+//        int [] b = {0, 1, 0, 1, 0, 0, 1, 1, 0};
+          int[] a = {1, 4, 2, 8};
+          int [] b = {0, 0, 0, 1};
 
-        int ans = solve(a, b, 8);
+        int ans = solve(a, b, 2);
         System.out.println(ans);
     }
     public static int solve(int[] a, int[] b, int c) {
         int ans = 0;
-        int[] arr = new int[a.length];
-        // find 1
+        // O(N) time | O(N) space
 
-        for (int i = 0; i < b.length; i++) {
-            if (b[i] == 1) {
-                ans += a[i];
-                System.out.println(a[i]);
-            }
-            else {
-                arr[i] = a[i];
-            }
-        }
-        System.out.println(ans);
-        System.out.println("BEFORE" + Arrays.toString(arr));
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        // Prefix sum
+        int[] pf = new int[a.length];
+        int[] pf1 = new int[a.length];
 
-        for (int i = arr.length - 1; i > arr.length - c - 2; i--) {
-            System.out.print("ANS BEFORE" + ans);
-            ans += arr[i];
-            System.out.print("ANS AFTER" + ans);
+        pf[0] = a[0];
+        if (b[0] == 1) pf1[0] = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            pf[i] = pf[i-1] + a[i];
+            if (b[i] == 1) pf1[i] = pf1[i-1] + a[i];
+            else pf1[i] = pf[i-1];
         }
 
-        return  ans;
-    }
+        System.out.println(Arrays.toString(pf));
+        System.out.println(Arrays.toString(pf1));
+        int l = 0, r = c-1, n = a.length;
+
+        while (r < n) {
+            int sum = 0;
+
+            //Middle
+            if (l == 0) {
+                sum = pf[r];
+            } else {
+                sum = pf[r] - pf[l-1];
+            }
+
+            //Left
+            if (l == 0) sum += pf1[0];
+            else sum += pf1[l-1];
+
+            // right
+            sum += pf1[n-1] - pf1[r+1-1];
+
+            ans = Math.max(ans, sum);
+
+            l++; r++;
+        }
+        return ans;
+     }
 }
--- ScalerContest/Contest3/PangramCheck.java
+++ ScalerContest/Contest3/PangramCheck.java
@@ -4,8 +4,8 @@ import java.util.HashSet;
 
 public class PangramCheck {
     public static void main(String[] args) {
-//        String[] arr = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
-        String[] arr = {"bit", "scale"};
+        String[] arr = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+//        String[] arr = {"bit", "scale"};
 
         int ans = solve(arr);
         System.out.println(ans);
@@ -13,18 +13,16 @@ public class PangramCheck {
     public static int solve(String[] arr) {
 
         HashSet<Character> hs = new HashSet<>();
-        int ans = 0;
 
         for (String str : arr) {
             for (int j = 0; j < str.length(); j++) {
                 if (!hs.contains(str.charAt(j))) {
                     hs.add(str.charAt(j));
-                    ans++;
                 }
             }
         }
         System.out.println(hs);
-        if (ans == 26) return  1;
+        if (hs.size() == 26) return 1;
         return 0;
     }
 }
