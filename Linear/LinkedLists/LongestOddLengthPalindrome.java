8ce7290f28a9797eebda45782da48a5411bd55a6
uday510 <admin@udayteja.com>
2023-05-09 22:50:15 +0530

    Add DoublyLinkedListConstruction Problem Solution

--- Linear/LinkedLists/LongestOddLengthPalindrome.java
+++ Linear/LinkedLists/LongestOddLengthPalindrome.java
@@ -10,12 +10,15 @@ public class LongestOddLengthPalindrome {
     }
     public static void main(String[] args) {
         ListNode tail = new ListNode(2, null);
-        ListNode node5 = new ListNode(1, tail);
+        ListNode node8 = new ListNode(2, tail);
+        ListNode node7 = new ListNode(3, node8);
+        ListNode node6 = new ListNode(1, node7);
+        ListNode node5 = new ListNode(2, node6);
         ListNode node4 = new ListNode(2, node5);
-        ListNode node3 = new ListNode(2,node4);
-        ListNode node2 = new ListNode(1, node3);
-        ListNode node1 = new ListNode(2, node2);
-        ListNode head = new ListNode(1, node1);
+        ListNode node3 = new ListNode(1, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        ListNode head = new ListNode(2, node1);
 
         int ans = solve(head);
         System.out.println(ans);
@@ -32,6 +35,8 @@ public class LongestOddLengthPalindrome {
             currLength = matchingCount(prevNode, nextNode);
             maxLength = Integer.max(currLength * 2 + 1, maxLength);
             currNode.next = prevNode;
+            currLength = matchingCount(currNode, nextNode);
+            maxLength = Integer.max(currLength * 2, maxLength);
             prevNode = currNode;
             currNode = nextNode;
         }
