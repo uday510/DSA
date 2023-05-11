494a173a4739cdc4b5626e3845633abcacd94d41
uday510 <admin@udayteja.com>
2023-05-11 16:42:56 +0530

    Add DoubleCharacterTrouble  Problem Solution

---
+++ Linear/Stacks/DoubleCharacterTrouble.java
@@ -0,0 +1,89 @@
+/**
+ * Problem Description
+ * You are given a string A.
+ *
+ * An operation on the string is defined as follows:
+ *
+ * Remove the first occurrence of the same consecutive characters. eg for a string "abbcd", the first occurrence of same consecutive characters is "bb".
+ *
+ * Therefore the string after this operation will be "acd".
+ *
+ * Keep performing this operation on the string until there are no more occurrences of the same consecutive characters and return the final string.
+ *
+ *
+ *
+ * Problem Constraints
+ * 1 <= |A| <= 100000
+ *
+ *
+ *
+ * Input Format
+ * First and only argument is string A.
+ *
+ *
+ *
+ * Output Format
+ * Return the final string.
+ *
+ *
+ *
+ * Example Input
+ * Input 1:
+ *
+ *  A = abccbc
+ * Input 2:
+ *
+ *  A = ab
+ *
+ *
+ * Example Output
+ * Output 1:
+ *
+ *  "ac"
+ * Output 2:
+ *
+ *  "ab"
+ *
+ *
+ * Example Explanation
+ * Explanation 1:
+ *
+ * Remove the first occurrence of same consecutive characters. eg for a string "abbc",
+ * the first occurrence of same consecutive characters is "bb".
+ * Therefore the string after this operation will be "ac".
+ * Explanation 2:
+ *
+ *  No removals are to be done.
+ */
+package Linear.Stacks;
+
+import java.util.Stack;
+
+public class DoubleCharacterTrouble {
+    public static void main(String[] args) {
+        String string = "abccbc";
+
+        String ans = solve(string);
+        System.out.println(ans);
+    }
+    public static String solve(String string) {
+        // O(N) time | O(N) space
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = string.length() - 1; i > -1; i--) {
+            char c = string.charAt(i);
+
+        if (stack.empty() || c != stack.peek()) {
+                stack.push(c);
+            } else {
+                stack.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+}
