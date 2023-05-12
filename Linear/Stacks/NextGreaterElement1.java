fa23d8289e774760518d90cc26392c67c9230fe3
uday510 <admin@udayteja.com>
2023-05-13 00:49:58 +0530

    Add Stack Problems Solution

---
+++ Linear/Stacks/NextGreaterElement1.java
@@ -0,0 +1,100 @@
+/**
+ * The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+ *
+ * You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+ *
+ * For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+ *
+ * Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+ * Output: [-1,3,-1]
+ * Explanation: The next greater element for each value of nums1 is as follows:
+ * - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+ * - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+ * - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+ * Example 2:
+ *
+ * Input: nums1 = [2,4], nums2 = [1,2,3,4]
+ * Output: [3,-1]
+ * Explanation: The next greater element for each value of nums1 is as follows:
+ * - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+ * - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 104
+ * All integers in nums1 and nums2 are unique.
+ * All the integers of nums1 also appear in nums2.
+ *
+ */
+package Linear.Stacks;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NextGreaterElement1 {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        int[] ans = solve(nums1, nums2);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] solve(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            hm.put(nums2[i], i);
+        }
+
+        int[] ngr = getNgr(nums2);
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int val = nums1[i];
+            int index = hm.get(val);
+            ans[i] = ngr[index];
+        }
+        return ans;
+    }
+    public static int[] getNgr(int[] array) {
+        int n = array.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i > -1; i--) {
+            int currNum = array[i];
+
+            if (stack.isEmpty()) {
+                stack.push(currNum);
+                ans[i] = -1;
+            }
+            else if (stack.peek() > currNum) {
+                ans[i] = stack.peek();
+                stack.push(currNum);
+            }
+            else {
+                while (!stack.isEmpty() && currNum >= stack.peek()) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    ans[i] = stack.peek();
+                } else {
+                    ans[i] = -1;
+                }
+                stack.push(currNum);
+            }
+
+        }
+        return ans;
+    }
+}
