1be550775bc06cccde410be86b4c49d1b98b7c03
uday510 <admin@udayteja.com>
2023-05-13 00:05:40 +0530

    Add Stack Problems Solution

---
+++ Linear/Stacks/BasicCalculator.java
@@ -0,0 +1,39 @@
+/**
+ * Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+ *
+ * Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "1 + 1"
+ * Output: 2
+ * Example 2:
+ *
+ * Input: s = " 2-1 + 2 "
+ * Output: 3
+ * Example 3:
+ *
+ * Input: s = "(1+(4+5+2)-3)+(6+8)"
+ * Output: 23
+ */
+package Linear.Stacks;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        String s = "1 + 1";
+
+//        int ans = solve(s);
+//        System.out.println(ans);
+    }
+//    public static Integer solve(String s) {
+//        Stack<Integer> stack = new Stack<>();
+//        ArrayList<Integer>
+//
+//        return stack;
+//    }
+}
