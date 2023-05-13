d2c1364a8447fa89e978ab1c4f2ffbf04e0d648f
uday510 <admin@udayteja.com>
2023-05-13 14:57:07 +0530

    Add BasicCalculator Problem Solution

--- Linear/Stacks/BasicCalculator.java
+++ Linear/Stacks/BasicCalculator.java
@@ -25,15 +25,44 @@ import java.util.Stack;
 
 public class BasicCalculator {
     public static void main(String[] args) {
-        String s = "1 + 1";
+        String s = "1+ 1";
 
-//        int ans = solve(s);
-//        System.out.println(ans);
-    }
-//    public static Integer solve(String s) {
-//        Stack<Integer> stack = new Stack<>();
-//        ArrayList<Integer>
-//
-//        return stack;
-//    }
+        int ans = solve(s);
+        System.out.println(ans);
+   }
+   public static int solve(String s) {
+        // O(N) time | O(N) space
+        int sum = 0;
+        int sign = 1;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // if the c is not digit continue
+            if (Character.isDigit(c)) {
+                int val = 0 ;
+                // get the digit , eg: 312
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    val = val * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                i--; // because of for loop
+                val = val*sign; // multiple with sign
+                sign = 1; // reset sign
+                sum += val; // add it to the total sum
+            } else if (c == '(') {
+                stack.push(sum);
+                stack.push(sign);
+                sum = 0;
+                sign = +1;
+            } else if (c == ')') {
+                sum *= stack.pop(); // sign
+                sum += stack.pop(); // digit
+            } else if (c == '-') {
+                sign *= -1;
+            }
+        }
+        return sum;
+   }
 }
