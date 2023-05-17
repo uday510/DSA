dd519be9060027ab4fd4026f1accbf054016c29c
uday510 <admin@udayteja.com>
2023-05-17 12:12:25 +0530

    Add Queue Problem Solution

--- Linear/Arrays/Subarrays/CountingSubArrays.java
+++ Linear/Arrays/Subarrays/CountingSubArrays.java
@@ -1,3 +1,5 @@
+package Linear.Arrays.Subarrays;
+
 /**
  * Given an array A of N non-negative numbers and a non-negative number B,
  * you need to find the number of subarrays in A with a sum less than B.
---
+++ Linear/Queues/QueueImplementationUsingLinkedList.java
@@ -0,0 +1,11 @@
+package Linear.Queues;
+
+public class QueueImplementationUsingLinkedList {
+    public static void main(String[] args) {
+        
+    }
+    /**
+     * Insert at tail
+     * Remove from head
+     */
+}
