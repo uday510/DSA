f5b35c9cb3cd8ced56f1ce87902997e2d8132e1d
uday510 <admin@udayteja.com>
2023-05-28 00:04:54 +0530

    Add LCAOfBinaryTree Problem Solution

--- NonLinear/BinaryTrees/LCAOfBinaryTree.java
+++ NonLinear/BinaryTrees/LCAOfBinaryTree.java
@@ -1,7 +1,5 @@
 package NonLinear.BinaryTrees;
 
-import java.util.ArrayList;
-
 public class LCAOfBinaryTree {
     public static class TreeNode {
         int val;
@@ -29,29 +27,80 @@ public class LCAOfBinaryTree {
             root.left = node1;
             root.right = node5;
 
-            ArrayList<Integer> ans = solve(root, 8);
-            System.out.println(ans);
-        }
-        public static ArrayList<Integer> solve(TreeNode root, int k) {
-            // O(N) time | O(N) space
-            ArrayList<Integer> arr = new ArrayList<>();
-            if (root == null) return arr;
-            helper(root, k, arr);
-            return arr;
-        }
-        public static boolean helper(TreeNode root, int k, ArrayList<Integer> arr) {
-            if (root == null) return false;
 
-            // add curr val to list
-            arr.add(root.val);
-            if (root.val == k) return true;
+            int p = 4;
+            int q = 6;
 
-            if ( helper(root.left, k, arr) || helper(root.right, k, arr) )
-                return true;
+            TreeNode ans = solve(root, p, q);
 
-            // remove curr val and return false, as we don't need this value
-            arr.remove(arr.size() - 1);
-            return false;
+            if (ans == null) System.out.println(ans);
+            else System.out.println(ans.val);
         }
+        public static TreeNode solve(TreeNode root, int p, int q) {
+            if (root == null || root.val == p || root.val == q) {
+                return root;
+            }
+
+            TreeNode left = solve(root.left, p, q);
+            TreeNode right = solve(root.right, p, q);
+
+            // result
+            if (left == null) {
+                return right;
+            } else if (right == null) {
+                return left;
+            } else {
+                // both left and right are not null, we found LCA
+                return root;
+            }
+        }
+        /**
+         * public static int solve(TreeNode root, int p, int q) {
+         *             // O(N) time | O(N) space
+         *             if (root == null) return 0;
+         *
+         *             ArrayList<Integer> list1 = new ArrayList<>();
+         *             ArrayList<Integer> list2 = new ArrayList<>();
+         *
+         *             helper(root, p, list1);
+         *             helper(root, q, list2);
+         *
+         *             System.out.println(list1);
+         *             System.out.println(list2);
+         *
+         *             int i = 0, j = 0;
+         *             int val = 0;
+         *
+         *             while (i < list1.size() && j < list2.size()) {
+         *
+         *                 if (list1.get(i) == list2.get(j)) {
+         *                     while (list1.get(i) == list2.get(j)) {
+         *                         val = list1.get(i);
+         *                         i++;
+         *                         j++;
+         *                     }
+         *                     break;
+         *                 }
+         *                 i++;
+         *                 j++;
+         *             }
+         *             return val;
+         *
+         *         }
+         *         public static boolean helper(TreeNode root, int k, ArrayList<Integer> arr) {
+         *             if (root == null) return false;
+         *
+         *             // add curr val to list
+         *             arr.add(root.val);
+         *             if (root.val == k) return true;
+         *
+         *             if ( helper(root.left, k, arr) || helper(root.right, k, arr) )
+         *                 return true;
+         *
+         *             // remove curr val and return false, as we don't need this value
+         *             arr.remove(arr.size() - 1);
+         *             return false;
+         *         }
+         */
     }
 }
