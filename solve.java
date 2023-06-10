64660f9939a154b23b181d93cf9cb4ec163d5365
uday510 <admin@udayteja.com>
2023-06-10 23:46:40 +0530

    Add FindMedianFromDataStream Problem Solution

---
+++ solve.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class solve {
+    public static void main(String[] args) {
+        String n = "0010";
+
+    }
+    public static void solve(int[] nums) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        List<Integer> list = new ArrayList<>();
+        maxHeap.add(nums[0]);
+        list.add(nums[0]);
+
+
+    }
+}
