58200b05525e743bf0e74a7c6f8b4ae5e4e2df78
uday510 <admin@udayteja.com>
2023-06-30 16:49:07 +0530

    Add CoinChange0 Problem Solution

---
+++ DynamicProgramming/CoinChange0.java
@@ -0,0 +1,59 @@
+/**
+ * You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+ *
+ * Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * Note: we can't use same coin twice
+ * Example 1:
+ *
+ */
+package DynamicProgramming;
+import java.util.Arrays;
+
+public class CoinChange0 {
+    public static void main(String[] args) {
+        int[] coins = {7, 4, 9, 6, 10, 13, 14, 11};
+        int amount = 22;
+
+        int res = optimized(coins, amount);
+        System.out.println(res);
+        System.out.println(coinChange(coins, amount));
+    }
+    public static int optimized(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = amount; j > -1; --j) {
+                if (j - coins[i-1] >= 0) {
+                    dp[j] = dp[j] + dp[j - coins[i-1]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+    public static int coinChange(int[] coins, int amount) {
+        // O(N*amount) time | O(N*amount) space
+        /**
+         * 1. {7, 9, 6}
+         * 2. {13, 9}
+         * 3. {11, 4, 7}
+         * total 3 ways
+         */
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        Arrays.fill(dp[0], 0); // Initialize first row with 0
+        for (int[] A : dp) A[0] = 1; // Initialize first column with 1
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (coins[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-coins[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+}
