97f5ced3410fb0b2d50f91852d6badd3b90554cd
Uday <admin@udayteja.com>
2023-07-06 02:17:23 +0530

    Add RegularExpression2 Problem Solution

--- DynamicProgramming/RegularExpression2.java
+++ DynamicProgramming/RegularExpression2.java
@@ -63,7 +63,7 @@ public class RegularExpression2 {
     public static void main(String[] args) {
         String A = "aab";
         String B = "c*a*b";
-        System.out.println(solve(A, B));
+        System.out.println(isMatch(A, B));
     }
     public static boolean solve(String A, String B) {
         return isMatch(A, B, A.length() - 1, B.length() - 1);
@@ -71,9 +71,9 @@ public class RegularExpression2 {
     public static boolean isMatch(String s, String pattern) {
         boolean[][] dp = new boolean[s.length() + 1][pattern.length() + 1];
         dp[0][0] = true;
-        for (int i = 1; i < dp[0].length; ++i) {
-            if (pattern.charAt(i - 1) == '*') {
-                dp[0][i] = dp[0][i - 2];
+        for (int j = 1; j < dp[0].length; ++j) {
+            if (pattern.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
             }
         }
         for (int i = 1; i < dp.length; ++i) {
