225087a90adf2af8c71e0c6e9008128ee66834cd
Uday <admin@udayteja.com>
2023-07-05 17:06:00 +0530

    Add RegularExpressionMatch Problem Solution

--- DynamicProgramming/RegularExpressionMatch.java
+++ DynamicProgramming/RegularExpressionMatch.java
@@ -71,6 +71,36 @@ public class RegularExpressionMatch {
         }
         return isMatch(s1, s2, s1.length() - 1, s2.length() - 1, dp) == 1;
     }
+    public static int isMatch(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1]; // dp[i][j] = 1 if s1[0...i] matches s2[0...j]
+        if (s1.length() == 0 && s2.length() == 0) { // both strings are empty
+            return 1;
+        }
+        if (s2.charAt(0) == '*') {
+            Arrays.fill(dp[0], 1);
+        } else {
+            Arrays.fill(dp[0], 0);
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) { // string is empty
+            for (int j = 1; j <= m; j++) { // pattern is empty
+                if (s2.charAt(j - 1) == '*') { // if pattern has '*'
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                } else if (s2.charAt(j - 1) == '?' || s1.charAt(i - 1) == s2.charAt(j - 1)) { // if pattern has '?' or both characters are same
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else { // if pattern has some other character
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
     public static int isMatch(String s1, String s2, int i, int j, int[][] dp) {
         if (i < 0 && j < 0) { // both strings are empty
             return 1;
