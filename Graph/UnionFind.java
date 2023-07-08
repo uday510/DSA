329bd7ca71094cc5dcf32620f865d4d74bc11222
Uday <admin@udayteja.com>
2023-07-08 14:16:20 +0530

    Add UnionFind Algorithm

--- Graph/UnionFind.java
+++ Graph/UnionFind.java
@@ -2,29 +2,12 @@ package Graph;
 
 public class UnionFind {
     private static int[] root;
-    public UnionFind(int size) {
-        root = new int[size];
-        for(int i = 0; i < size; ++i) {
-            root[i] = i;
-        }
-    }
 
-    public static int find(int x) {
-        return root[x];
-    }
-    public static void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            for (int i = 0; i < root.length; ++i) {
-                if (root[i] == rootY) {
-                    root[i] = rootX;
-                }
-            }
+    public UnionFind(int n) {
+        root = new int[n];
+        for (int i = 0; i < n; ++i) {
+            root[i] = i; // set root to itself as default
         }
-    }
-    public static boolean connected(int x, int y) {
-        return find(x) == find(y);
     }
 
     public static void main(String[] args) {
@@ -36,11 +19,29 @@ public class UnionFind {
         uf.union(6, 7);
         uf.union(3, 8);
         uf.union(8, 9);
-        System.out.println(uf.connected(1, 5)); // true
-        System.out.println(uf.connected(5, 7)); // true
-        System.out.println(uf.connected(4, 9)); // false
+        System.out.println(uf.connected(1, 5)); // 1
+        System.out.println(uf.connected(5, 7)); // 1
+        System.out.println(uf.connected(4, 9)); // 0
         // 1-2-5-6-7 3-8-9-4
         uf.union(9, 4);
-        System.out.println(uf.connected(4, 9)); // true
+        System.out.println(uf.connected(4, 9)); // 1
+    }
+    public int find(int x) {
+        // O(1) time | O(1) space
+        return root[x];
+    }
+    public void union(int x, int y) {
+        // O(N) time | O(1) space
+        int rootX = find(x); // find root of x
+        int rootY = find(y); // find root of y
+        for (int i = 0; i < root.length; ++i) {
+            if (root[i] == rootY) {
+                root[i] = rootX; // set root of y to root of x
+            }
+        }
+    }
+    public boolean connected(int x, int y) {
+        // O(1) time | O(1) space
+        return root[x] == root[y];
     }
 }
