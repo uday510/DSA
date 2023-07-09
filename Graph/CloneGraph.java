74bdb53238617316edf909e5a3bda4120d506fda
Uday <admin@udayteja.com>
2023-07-10 04:47:44 +0530

    Add CloneGraph Problem Solution

---
+++ Graph/CloneGraph.java
@@ -0,0 +1,115 @@
+/**
+ * Given a reference of a node in a connected undirected graph.
+ *
+ * Return a deep copy (clone) of the graph.
+ *
+ * Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+ *
+ * class Node {
+ *     public int val;
+ *     public List<Node> neighbors;
+ * }
+ *
+ *
+ * Test case format:
+ *
+ * For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+ *
+ * An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+ *
+ * The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+ *
+ *
+ *
+ * Example 1:
+ * Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+ * Output: [[2,4],[1,3],[2,4],[1,3]]
+ * Explanation: There are 4 nodes in the graph.
+ * 1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+ * 2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+ * 3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+ * 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+ *
+ * Input: adjList = [[]]
+ * Output: [[]]
+ * Explanation: Note that the input contains one empty list.
+ * The graph consists of only one node with val = 1 and it does not have any neighbors.
+ *
+ * Input: adjList = []
+ * Output: []
+ * Explanation: This an empty graph, it does not have any nodes.
+ *
+ * Constraints:
+ *
+ * The number of nodes in the graph is in the range [0, 100].
+ * 1 <= Node.val <= 100
+ * Node.val is unique for each node.
+ * There are no repeated edges and no self-loops in the graph.
+ * The Graph is connected and all nodes can be visited starting from the given node.
+ */
+package Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CloneGraph {
+    static private final HashMap<Node, Node> visited = new HashMap<>();
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2, new ArrayList<>());
+        Node node3 = new Node(3, new ArrayList<>());
+        Node node4 = new Node(4, new ArrayList<>());
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node4);
+        node2.neighbors.add(node1);
+        node2.neighbors.add(node3);
+        node3.neighbors.add(node2);
+        node3.neighbors.add(node4);
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+        Node node = cloneGraph(node1);
+        System.out.println(node);
+    }
+    public static Node cloneGraph(Node node) {
+        // O(N + M) time | O(N) space where N is the number of nodes and M is the number of edges
+        if (node == null) {
+            return null;
+        }
+        // If the node was already visited before.
+        // Return the clone from the visited map.
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        // Create a clone for the given node.
+        // Note that we don't have cloned neighbors as of now, hence[].
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        // The key is original node and value being the clone node.
+        visited.put(node, cloneNode);
+
+        // Iterate through the neighbors to generate their clones
+        // and prepare a list of cloned neighbors to be added to the cloned node.
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
+    }
+    static class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
