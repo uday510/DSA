3f295a22779b7222bb38937d1b7dab9890324321
Uday <admin@udayteja.com>
2023-07-14 01:08:33 +0530

    Add MaximumDepthOfBinaryTree Problem Solution

---
+++ Tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,55 @@
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf nod
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ */
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+    public static int maxDepth(TreeNode root) {
+        int count = 0;
+        if (root == null) {return count; }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            count++;
+            for (int i = 0; i < size; ++i) {
+                TreeNode currNode = queue.poll();
+                assert currNode != null;
+                if (currNode.left != null) queue.offer(currNode.left);
+                if (currNode.right != null) queue.offer(currNode.right);
+            }
+        }
+        return count;
+    }
+    public static int dfs(TreeNode root) {
+        if (root == null) {return 0; }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
