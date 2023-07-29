834d957489e73c961c81f4d2b4fc94e68cc7c8bd
Uday <admin@udayteja.com>
2023-07-29 14:34:36 +0530

    Add NumberOfClosedIslands Problem Solution

--- Graph/NumberOfIslands.java
+++ Graph/NumberOfIslands.java
@@ -1,7 +1,8 @@
 /**
  * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
  *
- * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
  *
  * Input: grid = [
  *   ["1","1","1","1","0"],
@@ -40,10 +41,8 @@ public class NumberOfIslands {
     }
     public static int numIslands(char[][] grid) {
         int[][] visited = new int[grid.length][grid[0].length];
-        visited[0][0] = 1;
         int numIslands = 0;
 
-        
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length;j++) {
                 if(grid[i][j] == '1' && visited[i][j] == 0) {
