3dfbf1a94fbb57df1257c2caa39f160350e8d65f
Uday <admin@udayteja.com>
2023-09-27 19:25:04 +0530

    Add SmallestSubsequence Problem Solution

---
+++ String/RemoveDuplicateLetters.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicateLetters {
+
+    public static String removeDuplicateLetters(String s) {
+        Set<Character> set = new HashSet<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        return new String();
+
+
+    }
+}
