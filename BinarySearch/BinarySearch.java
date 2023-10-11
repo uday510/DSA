0eab7836d0419ccaa2e3c334a5c247e37eedd40b
Uday <admin@udayteja.com>
2023-10-12 00:39:00 +0530

    Add FullBlowFlowers Problem Solution

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -8,49 +8,25 @@ public class BinarySearch {
         int[] array = {1, 2, 3, 3, 3, 3, 4, 5, 6};
         int target = 3;
         Arrays.sort(array);
-        System.out.println(binarySearch(array, target+1)-1);
+        System.out.println(binarySearch(array, target));
     }
 
     //TODO: STANDARD BINARY SEARCH TEMPLATE
 
     public static int binarySearch(int[] array, int target) {
-        int left = 0;
-        int right = array.length;
+        int left = 0, right = array.length - 1;
 
-        while (left < right) {
-            int midIdx = (left + right) / 2;
-            if (array[midIdx] < target) {
-                left = midIdx + 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else if (array[mid] > target) {
+                right = mid - 1;
             } else {
-                right = midIdx;
+                return mid;
             }
         }
-        return left;
-    }
-
-
-//    public static int binarySearch(int[] array, int target, int left, int right) {
-//        System.out.println("hi");
-//        // O(log(n)) time | O(1) space
-//        while(left <= right) {
-//            int mid = left + (right - left) >> 1;
-//            int potentialMatch = array[mid];
-//            System.out.println(mid);
-//            if(target == potentialMatch) return mid;
-//            else if(target < potentialMatch) left = mid + 1;
-//            else right = mid - 1;
-//        }
-//        return -1;
-//    }
-    public static int binarySearchRecursion(int array[], int target, int left, int right) {
-        // O(log(n)) time | O(log(n)) space
-        if(left > right) return -1;
-        int mid = left + (right - left) / 2;
-        int potentialMatch = array[mid];
-        if(target == potentialMatch) return mid;
-        else if(target < potentialMatch)
-            return binarySearchRecursion(array, target, left, mid - 1);
-        else
-            return binarySearchRecursion(array, target, mid + 1, right);
+        return -1;
     }
 }
---
+++ BinarySearch/BisectLeft.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class BisectLeft {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 4, 9};
+
+        System.out.println(bisectLeft(array, 1));
+    }
+
+    public static int bisectLeft(int[] array, int target) {
+        int left = 0, right = array.length;
+
+        while (left < right) {
+            int mid = (left + right) >> 1;
+
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left; // TODO : Trick to find the right most index
+    }
+}
