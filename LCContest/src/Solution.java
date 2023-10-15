910ea055b1d605030755a2fff1990990960bca61
Uday <admin@udayteja.com>
2023-10-15 09:01:04 +0530

    Add PaintingWalls Problem Solution

--- LCContest/src/Solution.java
+++ LCContest/src/Solution.java
@@ -1,34 +1,35 @@
-import java.util.*;
-
 class Solution {
 
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>(
-                List.of("1","2","prev","prev","prev"));
+        String s = "100011001";
+        int k = 3;
 
-        System.out.println(lastVisitedIntegers(words));
+        System.out.println(shortestBeautifulSubstring(s, k));
     }
-    public static List<Integer> lastVisitedIntegers(List<String> words) {
-        List<Integer> res = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
-        int k = 0;
-        for (int i = 0; i < words.size(); ++i) {
-            String word = words.get(i);
+    public static String shortestBeautifulSubstring(String s, int k) {
+        int n = s.length();
+        String result = "";
+        int minLength = n + 1;
 
-            if (!Objects.equals(word, "prev")) {
-                nums.add(Integer.parseInt(word));
-                k = 0;
-            } else {
-                if (i > 0 && Objects.equals(words.get(i-1), "prev")) {
-                    k++;
-                }
-                if (k >= nums.size()) {
-                    res.add(-1);
-                } else {
-                    res.add(nums.get(nums.size() - k - 1));
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                String substring = s.substring(i, j + 1);
+                int onesCount = countOnes(substring);
+                if (onesCount == k && substring.length() < minLength) {
+                    result = substring;
+                    minLength = substring.length();
                 }
             }
         }
-        return res;
+        return result;
+    }
+    private static int countOnes(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                count++;
+            }
+        }
+        return count;
     }
 }
