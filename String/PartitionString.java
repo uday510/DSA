7644d7d035f220e63566687b2240ebd19d4f0ecf
Uday <budayteja009@gmail.com>
2023-11-03 20:10:15 +0530

    Add PartitionString Problem Solution

---
+++ String/PartitionString.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.HashSet;
+
+public class PartitionString {
+    public static void main(String[] args) {
+
+    }
+    public static int partitionString(String s) {
+
+        boolean[] seen = new boolean[26];
+        int min = 1;
+
+        for (char c : s.toCharArray()) {
+            if (seen[c - 'a']) {
+                seen = new boolean[26];
+                min++;
+            }
+            seen[c - 'a'] = true;
+        }
+        return min;
+    }
+}
