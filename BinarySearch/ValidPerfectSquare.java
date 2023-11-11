f59310ee40cf0fb7d6f396a811372ea2d7453e67
Uday <budayteja009@gmail.com>
2023-11-11 14:45:42 +0530

    Add ValidPerfectSquare Problem Solution

--- BinarySearch/ValidPerfectSquare.java
+++ BinarySearch/ValidPerfectSquare.java
@@ -3,8 +3,7 @@ package BinarySearch;
 public class ValidPerfectSquare {
 
     public static boolean isPerfectSquare(int num) {
-        if (num < 2) return true;
-        long left = 2;
+        long left = 1;
         long right = num;
 
         while (left < right) {
