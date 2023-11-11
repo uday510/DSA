f964cb7d1c1cd5d0957b8880283deba95104ed10
Uday <budayteja009@gmail.com>
2023-11-11 13:16:00 +0530

    Add FindMinimumInRotatedSortedArray Problem Solution

--- BinarySearch/FindMinimumInRotatedSortedArray.java
+++ BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -44,57 +44,21 @@ public class FindMinimumInRotatedSortedArray {
         FindMinimumInRotatedSortedArray f = new FindMinimumInRotatedSortedArray();
         System.out.println(f.findMin(nums));
     }
-    public static int findMin(int[] nums) {
-//        int INFINITY = Integer.MAX_VALUE;
-//        int res = INFINITY;
-//
-//        for (int num : nums) {
-//            res = Math.min(res, num);
-//        }
-//
-//        return res == INFINITY ? Integer.MIN_VALUE : res;
+        public static int findMin(int[] nums) {
+            int left = 0;
+            int right = nums.length - 1;
 
-        if (nums.length == 1) {
-            return nums[0];
-        }
+            while (left < right) {
+                int mid = (left + right) >>> 1;
 
-        int left = 0, right = nums.length - 1;
-
-        // if the last element is greater than the first element then there is no
-        // rotation.
-        // e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
-        // Hence the smallest element is first element. A[0]
-
-        if (nums[right] > nums[0]) {
-            return nums[0];
-        }
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            // if the mid element is greater than its next element then mid+1 element is the
-            // smallest
-            // This point would be the point of change. From higher to lower value.
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
+                if (nums[mid] > nums[right]) {
+                    left = mid + 1;
+                } else if (nums[mid] < nums[right]) {
+                    right = mid;
+                } else {
+                    right--;
+                }
             }
-
-            if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
-            }
-
-            // if the mid elements value is greater than the 0th element this means
-            // the least value is still somewhere to the right as we are still dealing with elements
-
-            if (nums[mid] > nums[0]) {
-                left = mid + 1;
-            } else {
-                // if nums[0] is greater than the mid value then this means the smallest value is somewhere to
-                // the left
-                right = mid - 1;
-            }
+            return nums[left];
         }
-        return -1;
-    }
 }
