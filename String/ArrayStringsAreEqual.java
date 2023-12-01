a230fc650910d95923a074cf48f10e9e943bfc3d
Uday <budayteja009@gmail.com>
2023-12-01 11:07:24 +0530

    Add ArrayStringsAreEqual Problem Solution

---
+++ String/ArrayStringsAreEqual.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.util.Arrays;
+
+public class ArrayStringsAreEqual {
+    public static void main(String[] args) {
+
+    }
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        return String.join("", word1).equals(String.join("", word2));
+    }
+}
