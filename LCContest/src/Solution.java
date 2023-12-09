1fa9c88e6600d80513bdf8535576fbaf8553c8a9
Uday <budayteja009@gmail.com>
2023-12-09 22:36:56 +0530

    Add LeetCode Contest Solutions

--- LCContest/src/Solution.java
+++ LCContest/src/Solution.java
@@ -4,15 +4,15 @@ class Solution {
         String s = "100011001";
         int k = 3;
 
-        System.out.println(shortestBeautifulSubstring(s, k));
+//        System.out.println(shortestBeautifulSubstring(s, k));
     }
 
-    public static int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
-
-    }
-    public static int getMax(int n, int[] nums) {
-        
-
-
-    }
+//    public static int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+//
+//    }
+//    public static int getMax(int n, int[] nums) {
+//
+//
+//
+//    }
 }
