2858fcb95a4c862df5284c2ce5a51c8450431cad
Uday <budayteja009@gmail.com>
2024-02-07 08:17:35 +0530

    Add FrequencySort Problem Solution

--- LCContest/src/Solution.java
+++ LCContest/src/Solution.java
@@ -1,11 +1,10 @@
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 1, 2, 55, 14};
 
-        System.out.println(solve(arr));
+        System.out.println(maximumLength("abcaba"));
     }
 
      // Array : [1, 3, 4, 1, 2, 55, 14]
@@ -95,5 +94,40 @@ class Solution {
             this.next = next;
             this.val = val;
         }
+    }
+
+    public static int maximumLength(String s) {
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (char c = 'a'; c <= 'z'; ++c) {
+            list.add(new ArrayList<>());
+        }
+
+        int N = s.length();
+        for (int i = 0; i < N;) {
+            int j = i;
+            while (j < N && s.charAt(i) == s.charAt(j)) {
+                ++j;
+            }
+            int index = s.charAt(i) - 'a';
+            list.get(index).add(j - i);
+            i = j;
+        }
+
+        int res = -1;
+        for (List<Integer> frequency : list) {
+            int M = frequency.size();
+            if (M == 0) continue;
+            Collections.sort(frequency);
+            res = Math.max(res, frequency.get(M - 1) - 2);
+            if (M > 1) {
+                res = Math.max(res, Math.min(frequency.get(M - 1) - 1, frequency.get(M - 2)));
+            }
+            if (M > 2) {
+                res = Math.max(res, frequency.get(M - 3));
+            }
+        }
+        return res;
     }
 }
--- LeetCodeContest/Solution.java
+++ LeetCodeContest/Solution.java
@@ -1,4 +1,28 @@
 package LeetCodeContest;
 
+import Sort.HeapSort.MaxHeap;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
+    public static void main(String[] args) {
+    int[] nums = {1, 2, 3, 2, 5};
+
+        System.out.println(solve("a1b2c3d4"));
+    }
+    public static String solve(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            int num = 0;
+
+            int j = i;
+           if ( !(c >= 'a' && c <= 'z'))
+               System.out.println(c);
+        }
+        return sb.toString();
+    }
 }
