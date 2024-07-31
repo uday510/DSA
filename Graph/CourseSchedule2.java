a73d67c601aa036d54b8e72b125529d2b6d1dbb4
Uday <budayteja009@gmail.com>
2024-08-01 00:29:01 +0530

    Add MinHeightShelves Solution

--- Graph/CourseSchedule2.java
+++ Graph/CourseSchedule2.java
@@ -13,8 +13,8 @@
  * Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
  * So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
  */
-package Graph;
 
+package Graph;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
