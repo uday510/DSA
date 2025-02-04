62b20eae2567ad21ad4e247c0d8c512d5920c984
Uday <budayteja009@gmail.com>
2025-02-05 00:49:08 +0530

    Add MaximalSquare DP Solution

--- src/main/java/dynamicprogramming/MaximalSquare.java
+++ src/main/java/dynamicprogramming/MaximalSquare.java
@@ -42,4 +42,26 @@ public class MaximalSquare {
 
         return maximumSquare * maximumSquare;
     }
+
+    private static int maximalSquareOptimized(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maximum = 0;
+
+        for (int row = 1; row <= rows; ++row) {
+            for (int col = 1; col <= cols; ++col) {
+                if (matrix[row - 1][col - 1] == '0') {
+                    continue;
+                }
+
+                dp[row][col] = Math.min(dp[row - 1][col - 1],
+                        Math.min(dp[row - 1][col], dp[row][col - 1])) + 1;
+
+                maximum = Math.max(dp[row][col], maximum);
+            }
+        }
+
+        return maximum * maximum;
+    }
 }
