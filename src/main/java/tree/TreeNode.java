9098eb36afa308ad73cddf11566f50a888e4e77e
Uday <budayteja009@gmail.com>
2025-02-20 20:16:17 +0530

    Add UniqueBinarySearchTrees2 Solution With Dp

--- src/main/java/tree/TreeNode.java
+++ src/main/java/tree/TreeNode.java
@@ -2,14 +2,19 @@ package tree;
 
 
 public class TreeNode {
+
     public int val;
     public TreeNode left;
     public TreeNode right;
+
     TreeNode() {}
+
     public TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
+
 }
