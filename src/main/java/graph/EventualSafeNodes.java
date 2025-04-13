463352e7d7ebe8c3b74dfcfb38b5e1e86146a77d
Uday <budayteja009@gmail.com>
2025-04-13 12:35:37 +0530

    Fix EventualSafeNodes Solution

--- src/main/java/graph/EventualSafeNodes.java
+++ src/main/java/graph/EventualSafeNodes.java
@@ -4,49 +4,48 @@ import java.util.*;
 
 public class EventualSafeNodes {
     List<Integer>[] adjList;
-    Set<Integer> vis;
+    int[] states;
     int n;
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        intialize(graph);
+        initialize(graph);
         List<Integer> list = new ArrayList<>();
 
+        // DFS
         for (int node = 0; node < n; ++node) {
-            if (!vis.contains(node) && dfs(node)) {
+            if (dfs(node)) {
                 list.add(node);
             }
         }
 
-        Collections.sort(list);
         return list;
     }
 
     private boolean dfs(int node) {
-        vis.add(node);
-        for (int neighbour : adjList[node]) {
-            if (vis.contains(neighbour)) {
-                return false;
-            }
-            if (!dfs(neighbour)) {
-                return false;
-            }
-        }
+       if (states[node] != 0) return states[node] == 2;
 
-        return true;
+       states[node] = 1;
+       for (int neighbor : adjList[node]) {
+           if (!dfs(neighbor)) return false;
+       }
+
+       states[node] = 2;
+       return true;
     }
 
-    private void intialize(int[][] graph) {
+    private void initialize(int[][] graph) {
         int n = graph.length;
         adjList = new ArrayList[n];
-        vis = new HashSet<>();
+        states = new int[n];
 
         for (int i = 0; i < n; ++i) {
             adjList[i] = new ArrayList<>();
         }
 
        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < graph[i].length; ++j) {
-                adjList[i].add(graph[i][j]);
+            for (int vertex : graph[i]) {
+                adjList[i].add(vertex);
             }
        }
+
     }
 }
