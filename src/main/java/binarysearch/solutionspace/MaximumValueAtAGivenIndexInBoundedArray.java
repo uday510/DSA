5253f35e1cf819806c5a63433c7df969ce37a1ca
Uday Teja <dev@udayteja.com>
2025-08-16 17:19:24 +0530

    add solution-space solutions

--- src/main/java/binarysearch/solutionspace/MaximumValueAtAGivenIndexInBoundedArray.java
+++ src/main/java/binarysearch/solutionspace/MaximumValueAtAGivenIndexInBoundedArray.java
@@ -1,4 +1,54 @@
 package binarysearch.solutionspace;
 
 public class MaximumValueAtAGivenIndexInBoundedArray {
+
+    public int maxValue(int n, int index, int maxSum) {
+        int l = 1, r = maxSum;
+        int res = -1;
+
+        while (l <= r) {
+            int m = l + ((r - l) >> 1);
+
+            if (cnt(n, m, index) <= (long) maxSum) {
+                res = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return res;
+    }
+
+    private long cnt(int n, int val, int i) {
+        // AP sum formula:
+        // S_n = n/2 * (a1 + an)
+
+        long left;
+
+        // left side length = i
+        if (val <= i) {
+            // We hit 1 before filling i slots:
+            // sum(1..(val-1)) + remaining 1s
+            left = (long) (val - 1) * val / 2 + (i - (val - 1));
+        } else {
+            // Pure AP from (val-1) down to (val-i), i terms
+            left = (long) ((val - 1) + (val - i)) * i / 2;
+        }
+
+        long middle = val;
+
+        // right side length
+        int rlen = n - i - 1;
+        long right;
+        if (val <= rlen) {
+            // 1 before filling rlen slots
+            right = (long) (val - 1) * val / 2 + (rlen - (val - 1));
+        } else {
+            // Pure AP from (val-1) down to (val - rlen), rlen terms
+            right = (long) ((val - 1) + (val - rlen)) * rlen / 2;
+        }
+
+        return left + middle + right;
+
+    }
 }
--- src/main/java/binarysearch/solutionspace/MinimizeMaxDistanceToGasStation.java
+++ src/main/java/binarysearch/solutionspace/MinimizeMaxDistanceToGasStation.java
@@ -1,4 +1,29 @@
 package binarysearch.solutionspace;
 
 public class MinimizeMaxDistanceToGasStation {
+
+    public double minmaxGasDist(int[] stations, int k) {
+        double l = 0;
+        double r = 1e8;
+
+        while (r - l > 1e-6) {
+            double m = (l + r) / 2.0;
+            if (isPossible(m, stations, k)) r = m;
+            else l = m;
+        }
+
+        return l;
+    }
+
+    private boolean isPossible(double limit, int[] stations, int k) {
+        int curr = 0;
+
+        for (int i = 1; i < stations.length && curr <= k; ++i) {
+            double d = stations[i] - stations[i - 1];
+            if(d > limit) curr += (int) ( d/ limit);
+        }
+
+        return curr <= k;
+    }
+
 }
