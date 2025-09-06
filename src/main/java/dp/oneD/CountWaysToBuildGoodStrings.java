07d202643ddbf8af50ea2a69f9c7fae3021da03f
Uday <budayteja009@gmail.com>
2025-09-06 20:47:27 +0530

    Add CountWaysToBuildGoodStrings Solution

--- src/main/java/dp/oneD/CountWaysToBuildGoodStrings.java
+++ src/main/java/dp/oneD/CountWaysToBuildGoodStrings.java
@@ -4,32 +4,29 @@ import java.util.Arrays;
 
 public class CountWaysToBuildGoodStrings {
 
+    int l, h, z, o;
+    static int MOD = (int) 1e9 + 7;
     int[] dp;
-    int MOD = 1_000_000_007;
-    int low, high, zero, one;
 
     public int countGoodStrings(int low, int high, int zero, int one) {
-        this.low = low;
-        this.high = high;
-        this.zero = zero;
-        this.one = one;
-
-        dp = new int[high + 1];
+        l = low; h = high; z = zero; o = one;
+        dp = new int[h + 1];
         Arrays.fill(dp, -1);
 
         return dfs(0);
     }
 
-    private int dfs(int lengthSoFar) {
-        if (lengthSoFar > high) return 0;
-        if (dp[lengthSoFar] != -1) return dp[lengthSoFar];
+    private int dfs(int i) {
+        if (i > h) return 0;
 
-        int ways = (lengthSoFar >= low) ? 1 : 0;
+        if (dp[i] != -1) return dp[i];
 
-        ways = (ways + dfs(lengthSoFar + zero)) % MOD;
-        ways = (ways + dfs(lengthSoFar + one)) % MOD;
+        int cnt = i >= l ? 1 : 0;
 
-        return dp[lengthSoFar] = ways;
+        cnt = (cnt + dfs(i + z)) % MOD;
+        cnt = (cnt + dfs(i + o)) % MOD;
+
+        return dp[i] = cnt;
     }
 
 }
