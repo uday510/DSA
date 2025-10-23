5fae3f21cd0cd46f8c9d0b1d2bfd5f76eb769bf9
Uday Teja <dev@udayteja.com>
2025-10-23 10:34:40 +0530

    Add MaximalNetworkRank Solution

--- src/main/java/graph/MaximalNetworkRank.java
+++ src/main/java/graph/MaximalNetworkRank.java
@@ -1,42 +1,34 @@
 package graph;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class MaximalNetworkRank {
-    public static void main(String[] args) {
-        int n = 5;
-        int[][] roads = {{0,1},{0,3},{1,2},{1,3},{2,3},{2,4}};
-        System.out.println(maximalNetworkRank(n, roads));
-    }
-    static int maximalNetworkRank(int n, int[][] roads) {
 
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
+    public int maximalNetworkRank(int n, int[][] edges) {
+        Set<Integer>[] adjList = new HashSet[n];
 
-        for (int i = 0; i < n; i++) {
-            graph.put(i, new HashSet<>());
-        }
+        for (int i = 0; i < n; i++) adjList[i] = new HashSet<>();
 
-        for (int[] road: roads) {
-            int u = road[0];
-            int v = road[1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            adjList[u].add(v);
+            adjList[v].add(u);
         }
 
         int maxRank = 0;
-
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j <n; j++) {
-                int rank = graph.get(i).size() + graph.get(j).size();
-                if (graph.get(i).contains(j)) {
-                    rank--;
+            for (int j = i + 1; j < n; j++) {
+                int curRank = adjList[i].size() + adjList[j].size();
+                if (adjList[i].contains(j)) {
+                    curRank--;
                 }
-                maxRank = Math.max(maxRank, rank);
+
+                maxRank = Math.max(curRank, maxRank);
             }
         }
+
         return maxRank;
     }
+
 }
