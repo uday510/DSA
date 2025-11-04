e3b14b74c29b631d6646523003868637eb36a4be
Uday Teja <dev@udayteja.com>
2025-11-05 03:55:15 +0530

    Add DecodeWays2 Solution

--- src/main/java/dp/oneD/DecodeWays2.java
+++ src/main/java/dp/oneD/DecodeWays2.java
@@ -4,44 +4,51 @@ import java.util.Arrays;
 
 public class DecodeWays2 {
 
-    String s;
-    int n, mod;
-    int[] dp;
+    class Solution {
 
-    public int numDecodings(String s) {
-        this.s = s;
-        n = s.length();
-        mod = (int) 1e9 + 7;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
+        private String s;
+        private int n;
+        private long[] dp;
+        private final int MOD = (int) 1e9 + 7;
 
-        return dfs(0);
-    }
+        public int numDecodings(String s) {
+            this.s = s;
+            n = s.length();
+            dp = new long[n];
+            Arrays.fill(dp, -1);
 
-    private int dfs(int i) {
-        if (i >= n) return 1;
-
-        char ch1 = s.charAt(i);
-
-        int cnt = 0;
-        if (ch1 == '*') {
-            cnt = (9 + dfs(i + 1)) % mod;
+            return (int) dfs(0);
         }
 
-        if (i + 1 < n) {
-            char ch2 = s.charAt(i + 1);
+        private long dfs(int i) {
+            if (i >= n) return 1;
 
-            if (ch1 == '*' && ch2 == '*') {
-                cnt = (15 * dfs(i + 2)) % mod;
-            } else if (ch1 == '*') {
-                if (ch2 >= '1')
+            if (dp[i] != -1) return dp[i];
+
+            char c1 = s.charAt(i);
+            char c2 = i + 1 < n ? s.charAt(i + 1) : '#';
+
+            long cnt = 0;
+
+            if (c1 == '*') cnt = 9 * dfs(i + 1) % MOD;
+            else if (c1 >= '1') cnt = dfs(i + 1) % MOD;
+
+            if (c2 == '#') return dp[i] = cnt;
+
+            if (c1 == '*') {
+                if (c2 == '*') cnt += 15 * dfs(i + 2) % MOD;
+                else if (c2 >= '0' && c2 <= '6') cnt += 2 * dfs(i + 2) % MOD;
+                else cnt += dfs(i + 2) % MOD;
+            } else if (c1 == '1') {
+                if (c2 == '*') cnt += 9 * dfs(i + 2) % MOD;
+                else cnt += dfs(i + 2) % MOD;
+            } else if (c1 == '2') {
+                if (c2 == '*') cnt += 6 * dfs(i + 2) % MOD;
+                else if (c2 >= '0' && c2 <= '6') cnt += dfs(i + 2) % MOD;
             }
+
+            return dp[i] = cnt % MOD;
         }
-
-
-
-
-        return dp[i] = cnt;
     }
 
 }
